8cb349a4afb7ab25946bab8ca5182d8de3ba4aea
Jan Bil <jan.bil.personal@gmail.com>
2026-09-21 23:00:04 +0200

    feat: add ESLint configuration for Vue with TypeScript and Prettier

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import { globalIgnores } from "eslint/config";
+import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescript";
+import pluginVue from "eslint-plugin-vue";
+import prettier from "eslint-config-prettier";
+
+export default defineConfigWithVueTs([
+  globalIgnores(["dist", "storybook-static"]),
+  pluginVue.configs["flat/recommended"],
+  vueTsConfigs.recommended,
+  prettier,
+]);
