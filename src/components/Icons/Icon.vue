3271d089b78833b5053a98a1c850e93af088d4b8
Jan Bil <jan.bil.personal@gmail.com>
2026-09-24 13:27:17 +0200

    feat: add Icon component

---
+++ src/components/Icons/Icon.vue
@@ -0,0 +1,42 @@
+<template>
+  <span v-html="svg"></span>
+</template>
+<script lang="ts" setup>
+import { computed } from "vue";
+export type IconSize = "xl" | "lg" | "md" | "sm" | "xs";
+export type IconName =
+  | "check"
+  | "chevron-compact-down"
+  | "chevron-compact-up"
+  | "circle"
+  | "exclamation-mark"
+  | "hexagon"
+  | "horizontal-dots"
+  | "info-small"
+  | "maximize"
+  | "minimize"
+  | "minus"
+  | "plus"
+  | "vertical-dots"
+  | "x";
+
+const props = withDefaults(
+  defineProps<{
+    size?: IconSize;
+    name: IconName;
+  }>(),
+  {
+    size: "md",
+  },
+);
+
+const icons = import.meta.glob<string>("../../assets/icons/*.svg", {
+  query: "?raw",
+  import: "default",
+  eager: true,
+});
+
+const svg = computed(() => {
+    return icons[`../../assets/icons/icon-${props.name}.svg`],
+});
+</script>
