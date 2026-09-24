ed733410491895ce808038149d1ef811b656cfab
Jan Bil <jan.bil.personal@gmail.com>
2026-09-24 14:43:49 +0200

    feat: enhance Icon component with size classes and update SVG imports

--- playground/App.vue
+++ playground/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import Button from "../src/components/Button/Button.vue";
+import Icon from "../src/components/Icons/Icon.vue";
 </script>
 <template>
   <div class="bg-gray-500 h-100 w-100">
+    <Icon name="plus" size="xl"></Icon>
+    <Icon name="plus" size="xs"></Icon>
     <Button size="xl">click me</Button>
     <Button size="lg">click me</Button>
     <Button size="md">click me</Button>
     <Button size="sm">click me</Button>
-    <Button size="xs">click me</Button>
+    <Button size="xs">click me</Button>ś
   </div>
 </template>
