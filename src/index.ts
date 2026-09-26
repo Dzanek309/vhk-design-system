82feac7c5c86823d7c066ee6736cad41be28ab1e
Jan Bil <jan.bil.personal@gmail.com>
2026-09-26 18:00:54 +0200

    refactor(pill): add missing export type

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,9 @@ export { default as Icon } from "./components/Icon/Icon.vue";
 export type { IconSize } from "./components/Icon/Icon.vue";
 export type { IconName } from "./components/Icon/icon-names";
 export { default as Pill } from "./components/Pill/Pill.vue";
+export type {
+  PillSize,
+  PillShape,
+  PillPurpose,
+  PillVariant,
+} from "./components/Pill/Pill.vue";
